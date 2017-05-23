a4e5d6f992aa45d28e389f5a3f95bf4b036a4490
Jason <rhuidean06@gmail.com>
2017-05-22 23:37:12 -0500

    add money and outlier algorithm

---
+++ find_the_party_outlier.py
@@ -0,0 +1,12 @@
+'''You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns N.'''
+
+def find_outlier(integers):
+    ###
+    class OddEven(object):
+    	__init__(self):
+	    	self.odd_number=0
+	    	self.even_number=0
+	    	self.array_type=""
+	   	
+
+    
---
+++ money_money_money.py
@@ -0,0 +1,4 @@
+import numpy as np
+principal=[1000,1041,1083.86,1128.3]
+principal=np.array(principal)
+print principal.pop()
