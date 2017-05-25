3a4bd154200f84b274d39e9c9a63297810694937
Jason <rhuidean06@gmail.com>
2017-05-24 21:31:35 -0500

    add car escape algorithm

---
+++ car_park_escape.py
@@ -0,0 +1,20 @@
+def escape(carpark):
+    parking_floor=len(carpark)
+    count=0
+    for floor in range(parking_floor):
+    	if floor == parking_floor:
+    		staircase = carpark[floor].index(1)
+    		parking_lot = carpark[floor].index(2)
+    		moving_space=parking_lot-staircase
+    		case space <0
+    			"L"+str(moving_space)
+    		case space >0
+    			"R"+str(moving_space)
+
+    	elif floor == 0:
+    	
+    	else: 
+    		if carpark[floor].index(1) == staircase:
+    			count +=1
+
+    		elif:
