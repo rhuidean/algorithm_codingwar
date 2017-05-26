cfc5f680c6364b0fb400e100ca2da0ae0f454b34
Jason <rhuidean06@gmail.com>
2017-05-25 23:06:16 -0500

    add revised name list algorithm

--- namelist.py
+++ namelist.py
@@ -2,16 +2,19 @@ def namelist(names):
     length = len(names)
 
     if length == 0:
-        return ''
+        return "''"
         
     elif length == 1:
-        return names[0]
+        return "'"+names[0]['name']+"'"
     
     else:
     	string_list=[]
     	for object in names[:length-1]:
     		string_list.append(object['name'])
     	
-        return ", ".join(string_list) + " & " + names[length-1]['name']
+        return "'" + ", ".join(string_list) + " & " + names[length-1]['name'] + "'"
 
+print namelist([ {'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'} ])
 print namelist([ {'name': 'Bart'}, {'name': 'Lisa'}])
+print namelist([ {'name': 'Bart'}])
+print namelist([])
